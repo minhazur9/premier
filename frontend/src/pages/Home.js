672a1aa7a3dd8e5697a197403661ca6ff65b94cf
min <minhazur9@gmail.com>
2020-11-26 03:16:03 -0500

    Response data is now retrievable from react frontend

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import axios from 'axios';
 
 class Home extends React.Component {
+
+    componentDidMount() {
+        axios.get('http://localhost:8000/premier/profile')
+            .then((response) => console.log(response.data))
+    }
     render() {
         return (
-            <div>Hello</div>
+            <h1>Home</h1>
         )
     }
 }
--- premier/settings.py
+++ premier/settings.py
@@ -39,16 +39,22 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'main_app',
     'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
 ]
 
 ROOT_URLCONF = 'premier.urls'
