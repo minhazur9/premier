703adc659e58185757a14c00fb44a5a554074fec
min <minhazur9@gmail.com>
2020-11-26 21:35:25 -0500

    added mini details section on card

--- frontend/src/components/movies/MovieCard.js
+++ frontend/src/components/movies/MovieCard.js
@@ -4,7 +4,10 @@ class MovieCard extends React.Component {
     render() {
         return (
             <div className='movie-card'>
-                <div style={{backgroundImage: `url(${this.props.image})`}} className='card-poster'>  </div>
+                <div style={{backgroundImage: `url(${this.props.image})`}} className='card-poster'> 
+                <div className="movie-card-info"></div>
+                </div>
+                
             </div>
         )
     }
