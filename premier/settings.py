e156111debbd4eaa771b9813bec7729ce34183ac
min <minhazur9@gmail.com>
2020-12-10 18:01:46 -0500

    testing

--- premier/settings.py
+++ premier/settings.py
@@ -150,7 +150,7 @@ USE_TZ = True
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'frontend')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
