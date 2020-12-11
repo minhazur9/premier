2e60bda996548008752f2b10f4bb2e0bcc0e8293
min <minhazur9@gmail.com>
2020-12-10 20:03:48 -0500

    Added template view for home

--- premier/urls.py
+++ premier/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from rest_framework_jwt.views import obtain_jwt_token, verify_jwt_token, refresh_jwt_token
 from django.views.generic import TemplateView
 
