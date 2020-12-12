1f2c56fe188392847b773e8a931de6fb5e2e2570
min <minhazur9@gmail.com>
2020-12-11 21:50:56 -0500

    working on deploying

--- premier/settings.py
+++ premier/settings.py
@@ -150,11 +150,11 @@ USE_TZ = True
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'build' 'static'),
+    os.path.join(BASE_DIR, 'build/static'),
 )
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
